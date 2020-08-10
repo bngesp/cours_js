3960207babe2dcf43d0eed2fbda52cbc3d17a854
administrateur <bassirou.ngom@gitechssn.com>
2020-08-10 15:39:03 +0000

    dom

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+// var heure = prompt("Veuillez donner une heure");
+
+// var mondiv = document.querySelector(".heure");
+// mondiv.innerHTML = "L'heure est : "+heure;
+
+// var para2 = document.querySelector("#paragraphe2")
+// para2.style.fontSize = "34px"
