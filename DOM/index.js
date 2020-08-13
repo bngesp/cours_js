3ec62c27529739f7d580412fa687d9b1ba3dbd10
administrateur <bassirou.ngom@gitechssn.com>
2020-08-13 11:51:20 +0000

    Dom Event

--- DOM/index.js
+++ DOM/index.js
@@ -5,3 +5,16 @@
 
 // var para2 = document.querySelector("#paragraphe2")
 // para2.style.fontSize = "34px"
+
+var p1 = document.getElementById('paragraphe1');
+var p2 = document.getElementById('paragraphe2');
+
+function changerCouleur(){  
+    p1.classList.toggle('blue')
+    p2.classList.toggle('rouge')
+    p2.classList.toggle('blue')
+    p1.classList.toggle('rouge')
+}
+
+//window.setInterval(changerCouleur, 3000);
+
